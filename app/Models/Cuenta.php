de811b3d986c915dab50a661cd73457b8b12849e
Jaime García <jaime.garcia@iesdonana.org>
2024-12-11 20:07:49 +0100

    Migraciones modelos y relaciones

---
+++ app/Models/Cuenta.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cuenta extends Model
+{
+    /** @use HasFactory<\Database\Factories\CuentaFactory> */
+    use HasFactory;
+
+    public function cliente() {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function movimientos() {
+        return $this->hasMany(Movimiento::class);
+    }
+}
